9a11fbbe622acd66bb612beed77ca519b3a878c6
Alex Ma <alexma6614@gmail.com>
2019-04-19 11:48:46 +0800

    finished the spider

--- jdbspider/spiders/simplespider.py
+++ jdbspider/spiders/simplespider.py
@@ -9,7 +9,34 @@ from lxml import etree
 class SimplespiderSpider(CrawlSpider):
     name = 'simplespider'
     allowed_domains = ['hk.jobsdb.com']
-    start_urls = ['https://hk.jobsdb.com/hk/search-jobs/big-data/1']
+    start_urls = [
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/1',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/2',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/3',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/4',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/5',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/6',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/7',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/8',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/9',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/10',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/11',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/12',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/13',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/14',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/15',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/16',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/17',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/18',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/19',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/20',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/21',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/22',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/23',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/24',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/25',
+    'https://hk.jobsdb.com/hk/search-jobs/big-data/26'
+    ]
     user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"
 
     def parse(self, response):
@@ -27,5 +54,10 @@ class SimplespiderSpider(CrawlSpider):
         #}
 
         span_list = response.css("span::text").extract()
-        job['description'] = " ".join(span_list)
+        p_list = response.css("p::text").extract()
+        li_list = response.css("li::text").extract()
+        font_list = response.css("font::text").extract()
+        div_list = response.css("div::text").extract()
+        strong_list = response.css("strong::text").extract()
+        job['description'] = " ".join(span_list).strip('\r\n') + " ".join(p_list).strip('\r\n') + " ".join(li_list).strip('\r\n') + " ".join(font_list).strip('\r\n') + " ".join(div_list).strip('\r\n') + " ".join(strong_list).strip('\r\n')
         return job
